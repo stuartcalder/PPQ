d9461b693f175ce9503c39df738a58538128fda2
Stuart Calder <technoglub@protonmail.com>
2022-01-20 23:45:19 -0800

    Add more comments and reorder.

--- ubi512.h
+++ ubi512.h
@@ -26,14 +26,64 @@ typedef struct {
 	AL_ uint8_t	         msg_state   [SKC_THREEFISH512_BLOCK_BYTES];
 	uint64_t                 tweak_state [SKC_THREEFISH512_EXTERNAL_TWEAK_WORDS];
 } Skc_UBI512;
-
 #define SKC_UBI512_NULL_LITERAL (Skc_UBI512){0}
 
-SKC_API void Skc_UBI512_chain_config (R_(Skc_UBI512* const) ctx, const uint64_t num_out_bits);
-SKC_API void Skc_UBI512_chain_native_output (R_(Skc_UBI512* const) ctx, R_(uint8_t*) output);
-SKC_API void Skc_UBI512_chain_message (R_(Skc_UBI512* const) ctx, R_(const uint8_t*) input, uint64_t num_in_bytes);
-SKC_API void Skc_UBI512_chain_output (R_(Skc_UBI512* const) ctx, R_(uint8_t*) output, uint64_t num_out_byte);
-SKC_API void Skc_UBI512_chain_key (R_(Skc_UBI512* const) ctx, R_(const uint8_t*) input);
+/* Skc_UBI512_chain_config(context, num_output_bits)
+ *   @context:         Address of Skc_UBI512 struct.
+ *   @num_output_bits: Number of bits of outpit. (i.e. CHAR_BIT * (num output bytes)).
+ * No return; cannot fail.
+ */
+SKC_API void
+Skc_UBI512_chain_config
+(R_(Skc_UBI512* const) context,
+ const uint64_t        num_output_bits);
+
+/* Skc_UBI512_chain_message(context, input, num_input_bytes)
+ *   @context        : Address of Skc_UBI512 struct.
+ *   @input          : Address to read input bytes from.
+ *   @num_input_bytes: Number of bytes to read.
+ * No return; Cannot fail.
+ */
+SKC_API void
+Skc_UBI512_chain_message
+(R_(Skc_UBI512* const) context,
+ R_(const uint8_t*)    input,
+ uint64_t              num_input_bytes);
+
+/* Skc_UBI512_chain_output(context, output, num_output_bytes)
+ *   @context: Address of Skc_UBI512 struct.
+ *   @output : Address to write output bytes to.
+ *   @num_output_bytes: Number of bytes to write.
+ * No return; cannot fail.
+ */
+SKC_API void
+Skc_UBI512_chain_output
+(R_(Skc_UBI512* const) ctx,
+ R_(uint8_t*)          output,
+ uint64_t              num_out_byte);
+ 
+/* Skc_UBI512_chain_native_output(context, output)
+ *   @context: Address of Skc_UBI512 struct.
+ *   @output : Address to write 64 bytes of hash output.
+ * This procedure is functionally the same as $Skc_UBI512_chain_output
+ * when the @output_size is 64.
+ *
+ * No return; cannot fail.
+ */
+SKC_API void
+Skc_UBI512_chain_native_output
+(R_(Skc_UBI512* const) ctx,
+ R_(uint8_t*)          output);
+
+/* Skc_UBI512_chain_key(context, key_input)
+ *   @context  : Address of Skc_UBI512 struct.
+ *   @key_input: Address of 64-byte key, for MAC construction.
+ * No return; cannot fail.
+ */
+SKC_API void
+Skc_UBI512_chain_key
+(R_(Skc_UBI512* const) ctx,
+ R_(const uint8_t*)    input);
 BASE_END_C_DECLS
 #undef R_
 #undef AL_
