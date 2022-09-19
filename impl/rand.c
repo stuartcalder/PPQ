1dbd71146b91f2986aefb9da434caca7b4290afc
Stuart Calder <technoglub@protonmail.com>
2022-09-19 10:48:40 -0700

    Implementing generating pseudorandom integers within a range.

--- impl/rand.c
+++ impl/rand.c
@@ -2,6 +2,8 @@
  * See accompanying LICENSE file for licensing information.
  */
 #include "rand.h"
+#include "skein512.h"
+#define R_(Ptr) Ptr BASE_RESTRICT
 
 uint64_t Skc_rand_nat_num(Skc_CSPRNG* csprng, uint64_t nat_max)
 {
@@ -18,4 +20,23 @@ uint64_t Skc_rand_nat_num(Skc_CSPRNG* csprng, uint64_t nat_max)
   } else
     offset = num_sections - 1;
   return offset;
+}
+
+uint64_t Skc_rand_range_entropy(Skc_CSPRNG* csprng, const uint64_t min_val, const uint64_t max_val, R_(const char*) entropy_cstr)
+{
+  uint8_t buf[SKC_THREEFISH512_BLOCK_BYTES];
+
+  Base_assert_msg(min_val <= max_val, "Skc_rand_range_entropy: min_val > max_val!\n");
+
+  if (entropy_cstr != BASE_NULL) {
+    Skc_Skein512_hash_native(
+     &csprng->ubi512,
+     buf,
+     entropy_cstr,
+     strlen(entropy_cstr)
+    );
+    Skc_CSPRNG_reseed(csprng, buf);
+    Base_secure_zero(buf, sizeof(buf));
+  }
+  return min_val + Skc_rand_nat_num(csprng, max_val - min_val);
 }
--- rand.h
+++ rand.h
@@ -7,19 +7,24 @@
 #include "csprng.h"
 #include "macros.h"
 
-#define R_(ptr) ptr BASE_RESTRICT
-#define RAND_(Bits) \
-  BASE_INLINE void Skc_rand_uint##Bits(Skc_CSPRNG* csprng, uint##Bits##_t* buffer)\
-  {\
-    Skc_CSPRNG_get(csprng, buffer, sizeof(*buffer));\
-  }
+#define IMPL_(Bits) { Skc_CSPRNG_get(csprng, buffer, sizeof(*buffer)); }
 BASE_BEGIN_C_DECLS
-RAND_(16)
-RAND_(32)
-RAND_(64)
-SKC_API uint64_t Skc_rand_nat_num (Skc_CSPRNG* csprng, uint64_t nat_max);
+/* Store a pseudorandom uint16_t at @buffer. */
+BASE_INLINE uint16_t Skc_rand_uint16(Skc_CSPRNG* csprng, uint16_t* buffer) IMPL_(16)
+/* Store a pseudorandom uint32_t at @buffer. */
+BASE_INLINE uint32_t Skc_rand_uint32(Skc_CSPRNG* csprng, uint32_t* buffer) IMPL_(32)
+/* Store a pseudorandom uint64_t at @buffer. */
+BASE_INLINE uint64_t Skc_rand_uint64(Skc_CSPRNG* csprng, uint64_t* buffer) IMPL_(64)
+/* Store a pseudorandom natural number at most @nat_max. */
+SKC_API     uint64_t Skc_rand_nat_num(Skc_CSPRNG* csprng, uint64_t nat_max);
+/* Generate a pseudorandom uint64_t between a minimum and maximum inclusive.
+ * Optionally provide a C-string as entropy to the CSPRNG, or NULL otherwise. */
+SKC_API     uint64_t Skc_rand_range_entropy(Skc_CSPRNG* csprng, const uint64_t min_val, const uint64_t max_val, const char* BASE_RESTRICT entropy_cstr);
+/* Generate a pseudorandom uint64_t between a minimum and maximum inclusive. */
+BASE_INLINE uint64_t Skc_rand_range(Skc_CSPRNG* csprng, const uint64_t min_val, const uint64_t max_val) {
+  return Skc_rand_range_entropy(csprng, min_val, max_val, BASE_NULL);
+}
 BASE_END_C_DECLS
-#undef RAND_
-#undef R_
+#undef IMPL_
 
 #endif /* ~ ifndef SKC_RAND_H */
